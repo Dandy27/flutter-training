790ce7af7d2ae1304371279fb8260a2b7ae162a5
dandy <danielsbarbosa27@gmail.com>
2020-12-28 16:22:12 -0300

    Add Row

--- training_app/lib/HomePage.dart
+++ training_app/lib/HomePage.dart
@@ -9,7 +9,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int counter = 0;
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,8 +22,34 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Contador: $counter', style: TextStyle(fontSize: 27),),
+            Text(
+              'Contador: $counter',
+              style: TextStyle(fontSize: 27),
+            ),
             CustomSwitch(),
+            Container(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.green,
+                ),
+              ],
+            )
           ],
         ),
       ),
@@ -34,7 +59,8 @@ class _HomePageState extends State<HomePage> {
           setState(() {
             counter++;
           });
-        },) ,
+        },
+      ),
     );
   }
 }
@@ -42,10 +68,11 @@ class _HomePageState extends State<HomePage> {
 class CustomSwitch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Switch(value: AppController.instance.isDarkTheme,
-      onChanged: (value){
+    return Switch(
+      value: AppController.instance.isDarkTheme,
+      onChanged: (value) {
         AppController.instance.changeTheme();
-      },);
+      },
+    );
   }
 }
-
